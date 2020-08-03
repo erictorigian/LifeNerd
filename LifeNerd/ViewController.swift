d091303b75c0607076ce9156644d1bff523efa69
Eric Torigian <eric@torigian.com>
2020-08-03 13:20:15 -0400

    Inital commit

---
+++ LifeNerd/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LifeNerd
+//
+//  Created by Eric Torigian on 8/3/20.
+//  Copyright © 2020 Torigian Group, LLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
